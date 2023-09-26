af132ff78605fbd5b99394b404c2ab8fc93c26aa
Guybrush <miguel.barro@live.com>
2023-09-26 16:24:33 +0200

    Mqtt desktop client demo. Mqtt loop barebones
    Signed-off-by: Guybrush <miguel.barro@live.com>

--- mqtt/desktop_proto_client/src/main.cpp
+++ mqtt/desktop_proto_client/src/main.cpp
@@ -1,34 +1,105 @@
+#include <cstring>
+
 #include <mosquittopp.h>
 #include <absl/log/log.h>
+
+// TODO: set up user Ctrl-C
+bool user_exit = false;
+
+// pub/sub according with esp32 client not this one
+static const char* subscriber_topic = "esp32/gps/subscribe";
+static const char* publisher_topic = "esp32/gps/publish";
 
 class mqtt_client :
     public mosqpp::mosquittopp
 {
+    mqtt_client()
+    {
+        if (MOSQ_ERR_UNKNOWN == mosqpp::lib_init())
+            LOG(ERROR) << "Cannot initialize MQTT";
+    }
+
+    public:
+
+    ~mqtt_client()
+    {
+		unsubscribe(nullptr, publisher_topic);
+        mosqpp::lib_cleanup();
+    }
+
+    static mqtt_client& get_client()
+    {
+        static mqtt_client client;
+        return client;
+    }
+
     protected:
 
-//    void on_connect(int /*rc*/) override;
-//    void on_connect_with_flags(int /*rc*/, int /*flags*/) override;
-//    void on_disconnect(int /*rc*/) override;
-//    void on_publish(int /*mid*/) override;
-//    void on_message(const struct mosquitto_message * /*message*/) override;
-//    void on_subscribe(int /*mid*/, int /*qos_count*/, const int * /*granted_qos*/) override;
-//    void on_unsubscribe(int /*mid*/) override;
-//    void on_log(int /*level*/, const char * /*str*/) override;
-//    void on_error() override;
+    void on_connect(int rc) override
+    {
+        // https://docs.oasis-open.org/mqtt/mqtt/v3.1.1/errata01/os/mqtt-v3.1.1-errata01-os-complete.html#_Toc385349256
+        switch(rc)
+        {
+            case 0:
+                LOG(INFO) << "Connection accepted";
+
+                // launch the subscription
+                if (MOSQ_ERR_SUCCESS != subscribe(nullptr, publisher_topic))
+                {
+                    LOG(ERROR) << "Cannot set up the subscription";
+                    // exit the loop
+                    disconnect();
+                }
+            break;
+            defautl:
+                LOG(ERROR) << "Connection rejected with code: " << rc;
+        }
+    }
+
+    void on_subscribe(int mid, int /*qos_count*/, const int * /*granted_qos*/) override
+    {
+        LOG(INFO) << "Subscription accepted";
+    }
+
+    void on_message(const mosquitto_message* msg) override
+    {
+        if(std::strcmp(msg->topic, publisher_topic))
+        {
+            LOG(ERROR) << "Unexpected topic message " << msg->topic;
+        }
+        else
+        {
+            // TODO: deserialize magic
+        }
+    }
 };
 
 int main(int argc, char* argv[])
 {
-    mqtt_client client;
-
     //TODO: set up cli
     const char* host = "DESKTOP-BARRO";
     const int port = 6338;
 
     // Set up connection
-    client.connect(host, port, true);
+    mqtt_client& client = mqtt_client::get_client();
 
+    if (MOSQ_ERR_SUCCESS != client.connect(host, port, true))
+    {
+        LOG(ERROR) << "Cannot establish connection";
+        return -1;
+    }
 
+    while(!user_exit)
+    {
+        // keep looping and publishing each second
+        int res = client.loop_forever(1000);
+
+        if (MOSQ_ERR_SUCCESS == res)
+        {
+            // TODO: generate the message magic
+            // client.publish(nullptr, subscriber_topic, len, payload);
+        }
+    }
 
     return 0;
 }
